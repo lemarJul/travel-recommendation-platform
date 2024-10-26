4d4f3ec6c0ac4fd285db97713b7168dc0ff5e72c
Julien Lemarchand <lemar.jul@gmail.com>
2024-10-26 11:05:24 +0200

    fix fetch url

--- fetchData.js
+++ fetchData.js
@@ -5,7 +5,7 @@
  */
 async function fetchTravelData() {
   try {
-    const response = await fetch("/travel_recommendation_api.json");
+    const response = await fetch("../travel_recommendation_api.json");
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
